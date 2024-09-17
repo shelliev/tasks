58aea54cdb205e165faa634835ac3d6217150950
shellie <shelliev@udel.edu>
2024-09-17 14:22:50 -0400

    test passes2

--- src/objects.ts
+++ src/objects.ts
@@ -44,10 +44,10 @@ export function isCorrect(question: Question, answer: string): boolean {
 export function isValid(question: Question, answer: string): boolean {
     if (question.type === "short_answer_question") {
         return true;
-    } else if (question.type === "multiple_choice_question") {
+    } else {
         return question.options.includes(answer);
     }
-    return false;
+    // return false;
 }
 
 /**
