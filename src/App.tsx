e2e6ff9e2f3c87b1af764d78e4dce41bd71d3bbe
shellie <shelliev@udel.edu>
2024-10-06 22:42:23 -0400

    ColoredBox passes tests

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
-//import { ColoredBox } from "./bad-components/ColoredBox";
+import { ColoredBox } from "./bad-components/ColoredBox";
 //import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 
@@ -23,9 +23,9 @@ function App(): React.JSX.Element {
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
-            {/* <ColoredBox></ColoredBox>
+            <ColoredBox></ColoredBox>
             <hr></hr>
-            <ShoveBox></ShoveBox> */}
+            {/* <ShoveBox></ShoveBox> */}
             <hr></hr>
             <Counter></Counter>
             <hr />
