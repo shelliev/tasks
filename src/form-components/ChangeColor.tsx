0ab77a0a61afff1f35209e0ba2349fdc6e356e06
shellie <shelliev@udel.edu>
2024-10-19 16:56:37 -0400

    changecolor

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { FormCheck } from "react-bootstrap";
 
 export function ChangeColor(): React.JSX.Element {
+    const colors = [
+        "pink",
+        "purple",
+        "white",
+        "black",
+        "blue",
+        "orange",
+        "green",
+        "brown",
+    ];
+    const [aColor, setColor] = useState<string>(colors[0]);
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((color, index) => (
+                <FormCheck
+                    type="radio"
+                    name="color"
+                    id={`color-check-${color}`}
+                    label={color}
+                    value={color}
+                    checked={aColor === color}
+                    onChange={updateColor}
+                    key={index}
+                />
+            ))}
+            <div
+                data-testid="colored-box" // Fixed the placement of data-testid
+                style={{
+                    width: "50%",
+                    height: "50%",
+                    backgroundColor: aColor,
+                }}
+            >
+                {aColor}
+            </div>
         </div>
     );
 }
